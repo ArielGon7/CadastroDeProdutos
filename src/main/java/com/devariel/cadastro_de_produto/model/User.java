53c5e5ed3124c6b6df5290eedf54332743c427d1
ArielGon7 <arielgomessantos7@gmail.com>
2026-02-11 23:09:52 -0300

    Criação do Entity User.

---
+++ src/main/java/com/devariel/cadastro_de_produto/model/User.java
@@ -0,0 +1,26 @@
+package com.devariel.cadastro_de_produto.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
