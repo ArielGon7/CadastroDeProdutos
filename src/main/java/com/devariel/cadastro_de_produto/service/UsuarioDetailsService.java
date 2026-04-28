c16f92d874c0c8efa51ba15113dc31ad036fcc7e
ArielGon7 <arielgomessantos7@gmail.com>
2026-04-27 21:37:24 -0300

    Implementação da classe UsuarioDetailsService.java

---
+++ src/main/java/com/devariel/cadastro_de_produto/service/UsuarioDetailsService.java
@@ -0,0 +1,26 @@
+package com.devariel.cadastro_de_produto.service;
+
+import com.devariel.cadastro_de_produto.model.User;
+import com.devariel.cadastro_de_produto.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class UsuarioDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
